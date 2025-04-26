20e1e54a0e84e3c615e82eecd1e3721d3db34870
Daniil T <daniil.arimed@gmail.com>
2025-04-26 21:14:48 +0700

    Bech32Engine make it public

--- PlatONet/crypto/Bech32Engine.cs
+++ PlatONet/crypto/Bech32Engine.cs
@@ -29,7 +29,7 @@ using System.Linq;
 
 namespace PlatONet.Crypto
 {
-    internal static class Bech32Engine
+    public static class Bech32Engine
     {
         // used for polymod
         private static readonly uint[] generator = { 0x3b6a57b2, 0x26508e6d, 0x1ea119fa, 0x3d4233dd, 0x2a1462b3 };
